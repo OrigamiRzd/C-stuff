5dff15eadd504202af37e0c2fccc13061fc8257e
rzd83091 <rzd83091@uga.edu>
2015-08-26 18:01:16 -0400

    Create main.cpp
    new new

---
+++ Multifile-Example/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int add(int,int);     //in add.cpp
+int line(); 
+
+int main()
+{
+int x=line()
+int y=line()
+std::cout << "x + y = " add(y,x);
+return 0;
+}
+
+int line()
+{
+int x
+std::cout << "Enter in an integer: ";
+std::cin >> x;
+return x;
+}
